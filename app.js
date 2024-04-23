ba49cfdbc055a12b650174e4a407815d9e0976fe
Alihh961 <hajhassan.ali@outlook.com>
2024-04-23 21:43:54 +0200

    intial commit , adding nodeJS , Express ,ejs engine and home page

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require ('express');
+
+
+
+const app = express();
+
+app.set('view engine' , 'ejs');
+app.use(express.static('./public')); // to have the access to static files in the browser
+
+
+app.use('/' , ( req , res)=>{
+    res.render('home');
+})
+
+
+
+module.exports = app;
